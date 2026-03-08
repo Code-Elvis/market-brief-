d2a79d4c8dff267f3762696c5982d472fc2d5166
Code-Elvis <elvisgodbless@gmail.com>
2026-03-08 13:03:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import { copyFileSync } from 'fs'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'copy-app-html',
-      closeBundle() {
-        copyFileSync(
-          resolve(__dirname, 'dist/app.html'),
-          resolve(__dirname, 'dist/index.html')
-        )
-      }
-    }
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
       input: {
+        main: resolve(__dirname, 'index.html'),
         app: resolve(__dirname, 'app.html'),
       }
     }
