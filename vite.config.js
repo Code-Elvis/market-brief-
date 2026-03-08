2a7e7503cb47747cb5953bb19ddc3572d57107b9
Code-Elvis <elvisgodbless@gmail.com>
2026-03-08 14:34:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    {
+      name: 'copy-landing',
+      closeBundle() {
+        fs.copyFileSync(
+          resolve(__dirname, 'landing.html'),
+          resolve(__dirname, 'dist/landing.html')
+        )
+      }
+    }
+  ]
 })
