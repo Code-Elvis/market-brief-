fe2e33b773abc5e81e7273011b01e54b6177be08
Code-Elvis <elvisgodbless@gmail.com>
2026-03-10 01:44:57 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
 if (!PUBLISHABLE_KEY) {
   throw new Error("Missing Clerk publishable key");
 }
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      proxyUrl="https://marketdebriefs.com/__clerk"
+      afterSignOutUrl="/"
+    >
       <App />
     </ClerkProvider>
   </StrictMode>
