be54ca4016f0e10124b890afa0c37d583125315c
Code-Elvis <elvisgodbless@gmail.com>
2026-03-11 13:06:02 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App.jsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-if (!PUBLISHABLE_KEY) throw new Error("Missing Clerk publishable key");
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Clerk publishable key");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
